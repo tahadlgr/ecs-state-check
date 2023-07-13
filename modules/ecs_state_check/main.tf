54a437dc72516739fa9dc47f61963f913839592b
Cagrihan Dulgar <taha_chd@outlook.com>
2023-07-13 15:25:54 +0300

    Fix for lambda permissions

--- modules/ecs_state_check/main.tf
+++ modules/ecs_state_check/main.tf
@@ -156,10 +156,32 @@ data "aws_iam_policy_document" "lambda_policy" {
 
 }
 
-resource "aws_iam_role_policy" "lambda_policy" {
-  name   = "ecs-sc-policy"
-  role   = aws_iam_role.this.name
-  policy = data.aws_iam_policy_document.lambda_policy.json
+data "aws_iam_policy_document" "lambda_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecs:DescribeTasks",
+      "iam:ListAccountAliases",
+      "organizations:ListAccounts",
+      "organizations:DescribeAccount"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = ["${aws_cloudwatch_log_group.this.arn}:*"]
+  }
+
+  statement {
+    effect    = "Allow"
+    actions   = ["sts:AssumeRole"]
+    resources = [for account_id in var.all_account_ids : "arn:aws:iam::${account_id}:role/ecs-api-access-cross-account-role"]
+  }
 }
 
 # Lambda
